9a79d6ed019bd865c7dc5aaeaeee3748115a3d71
David Whimple <davidwhimple@fullsail.edu>
2013-02-15 12:17:35 -0800

    Added files for Project 4 and put in code for test data.

---
+++ Whimple_David_Project4/JSON.js
@@ -0,0 +1,82 @@
+var json = {
+	"log1": {
+		"date": ["Date:", "2012-02-27"],
+		"weight": ["Weight:", "220"],
+		"select": ["Meal:", "Breakfast"],
+		"food1": ["Food:", "Eggs"],
+		"foodCals1": ["Calories:", "30"],
+		"range1": ["Serving(s):", "3"],
+		"food2": ["Food:", "Toast"],
+		"foodCals2": ["Calories:", "100"],
+		"range2": ["Serving(s):", "2"],
+		"food3": ["Food:", "Bacon"],
+		"foodCals3": ["Calories:", "80"],
+		"range3": ["Serving(s):", "2"],
+		"food4": ["Food:", "Milk"],
+		"foodCals4": ["Calories:", "60"],
+		"range4": ["Serving(s):", "1"],
+		"check1": ["","Notes:"],
+		"notes": ["", "I love breakfast."]
+
+	},
+	"log2": {
+		"date": ["Date:", "2012-03-27"],
+		"weight": ["Weight:", "210"],
+		"select": ["Meal:", "Lunch"],
+		"food1": ["Food:", "Sandwich"],
+		"foodCals1": ["Calories:", "220"],
+		"range1": ["Serving(s):", "1"],
+		"food2": ["Food:", "Apple"],
+		"foodCals2": ["Calories:", "40"],
+		"range2": ["Serving(s):", "Chips"],
+		"food3": ["Food:", "Eggs"],
+		"foodCals3": ["Calories:", "100"],
+		"range3": ["Serving(s):", "1"],
+		"food4": ["Food:", "Dr.Pepper"],
+		"foodCals4": ["Calories:", "220"],
+		"range4": ["Serving(s):", "1"],
+		"check1": ["","Notes:"],
+		"notes": ["", "I don't feel as sluggish but I probably shouldn't have had that soda."]
+	},
+	"log3": {
+		"date": ["Date:", "2012-04-27"],
+		"weight": ["Weight:", "200"],
+		"select": ["Meal:", "Dinner"],
+		"food1": ["Food:", "Spaghetti"],
+		"foodCals1": ["Calories:", "300"],
+		"range1": ["Serving(s):", "1"],
+		"food2": ["Food:", "Garlic Bread"],
+		"foodCals2": ["Calories:", "200"],
+		"range2": ["Serving(s):", "1"],
+		"food3": ["Food:", "Wine"],
+		"foodCals3": ["Calories:", "60"],
+		"range3": ["Serving(s):", "3"],
+		"food4": ["Food:", ""],
+		"foodCals4": ["Calories:", ""],
+		"range4": ["Serving(s):", ""],
+		"check1": ["",""],
+		"notes": ["", ""]
+
+	},
+	"log4": {
+		"date": ["Date:", "2012-05-27"],
+		"weight": ["Today's Weight:", "195"],
+		"select": ["Meal:", "Snacks"],
+		"food1": ["Food:", "Gummy Bears"],
+		"foodCals1": ["Calories:", "35"],
+		"range1": ["Serving(s):", "3"],
+		"food2": ["Food:", "Salad"],
+		"foodCals2": ["Calories:", "120"],
+		"range2": ["Serving(s):", "1"],
+		"food3": ["Food:", "licorice"],
+		"foodCals3": ["Calories:", "20"],
+		"range3": ["Serving(s):", "3"],
+		"food4": ["Food:", "New Castle"],
+		"foodCals4": ["Calories:", "200"],
+		"range4": ["Serving(s):", "12"],
+		"check1": ["","Notes:"],
+		"notes": ["", "Holy crap. I drank too much beer."]
+	}
+};
+
+
