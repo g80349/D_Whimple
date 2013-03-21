4f27839dcb82682e58453c381bc2f53e5c326c87
David Whimple <davidwhimple@fullsail.edu>
2013-03-21 11:14:33 -0700

    Added required to appropriate fields and placeholders.

--- MIU/Whimple_David_Project3/Gold/js/main.js
+++ MIU/Whimple_David_Project3/Gold/js/main.js
@@ -1,7 +1,7 @@
 $('#home').on('pageinit', function(){
 	//code needed for home page goes here
-});	
-		
+});
+
 $('#addItem').on('pageinit', function(){
 
 		var myForm = $('#form1');
@@ -13,13 +13,13 @@ $('#addItem').on('pageinit', function(){
 			storeData(data);
 		}
 	});
-		
-		var clear = document.getElementById("clear");					
-		clear.addEventListener("click", clearLocal);  
+
+		var clear = document.getElementById("clear");
+		clear.addEventListener("click", clearLocal);
 		var displayData = document.getElementById("displayPage");
 		displayData.addEventListener("click", getData);
 
-	
+
 });
 
 //The functions below can go inside or outside the pageinit function for the page in which it is needed.
@@ -55,30 +55,38 @@ var getData = function(){
 			for(var x in infoObj){
 				var createSubLi = document.createElement("li");
 				createSubList.appendChild(createSubLi);
-				
+
 				var subText = infoObj[x][0]+ " " + infoObj[x][1];
 				createSubLi.innerHTML = subText;
-				createSubList.appendChild(linkList);	
-			}	
+				createSubList.appendChild(linkList);
+			}
 //		createLinks(localStorage.key(i), linkList);
-		}	
+		}
 };
 
 
 var storeData = function(data){
-//	if(!key){
+	if(!key){
 		var uniqueId = Math.floor(Math.random()*1000000000);
-//		}else{
-//			uniqueId = key;
-//		};
+		}else{
+			uniqueId = key;
+		};
 		localStorage.setItem(uniqueId, JSON.stringify(data));
 		alert("Log saved!");
-}; 
+};
 
 var	deleteItem = function (){
-			
+		var question = confirm("Are you sure you want to delete this log?");
+			if(question){
+				localStorage.removeItem(this.key);
+				alert("Log deleted.");
+				window.location.reload();
+			}else{
+				alert("Log was NOT deleted.");
+				return false;
+			}
 };
-					
+
 var clearLocal = function(){
 	var x = confirm("Are you sure you want to clear ALL of your food logs?");
 	if(x == true){
