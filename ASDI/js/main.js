f7abdf105192404bbb0c11fe929dc6945c09a151
David Whimple <davidwhimple@fullsail.edu>
2013-05-08 14:32:00 -0700

    Put in pageint for js

--- ASDI/js/main.js
+++ ASDI/js/main.js
@@ -2,3 +2,17 @@
 //ASDI 1305
 //Project 1
 //Javascript
+
+
+$('#home').on('pageinit', function(){
+	//code needed for home page goes here
+});
+
+$('#addItem').on('pageinit', function(){
+	//code needed for addItem page goes here
+});
+
+$('#display').on('pageinit', function(){
+	//code needed for display page goes here
+});
+
