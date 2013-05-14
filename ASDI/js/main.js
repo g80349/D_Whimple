3f5b3a87ddb1143ec12da6e1ae1195f0905d2ac5
David Whimple <davidwhimple@fullsail.edu>
2013-05-14 11:33:36 -0700

    Working on displaying local storage

--- ASDI/js/main.js
+++ ASDI/js/main.js
@@ -9,10 +9,36 @@ $('#home').on('pageinit', function(){
 });
 
 $('#addItem').on('pageinit', function(){
-	//code needed for addItem page goes here
-});
 
+	$('#logFood').on("click", function(){
+		var data = $('#form').serializeArray();
+		var id = Math.floor(Math.random()*1000000000);
+	    localStorage.setItem(id, JSON.stringify(data));
+		alert("Log saved!");
+	});
+
+
+	$("#clear").on("click",function(){
+		localStorage.clear();
+	});
+
+});
 $('#display').on('pageinit', function(){
-	//code needed for display page goes here
+	if(localStorage.length === 0){
+		alert("You have not logged any food so default logs were added.");
+	}
+	for(i = 0; i < localStorage.length; i++){
+		$('#displayList').append('<ul> <li> <ul>');
+		var key = localStorage.key(i);
+		var value = localStorage.getItem(key);
+		var infoObj = JSON.parse(value);
+		for(var x in infoObj){
+			if(infoObj[x].value !== "" && infoObj[x].value !== undefined){
+			var subText = infoObj[x].name + " " + infoObj[x].value;
+			$('<li>').appendTo('#displayList ul li ul').text(subText);
+			console.log(subText);
+			}
+		}
+		}
 });
 
